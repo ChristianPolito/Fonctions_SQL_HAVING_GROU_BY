e82c4f890978df35a6e7751366980523424c9ca4
Christian Polito <54096839+ChristianPolito@users.noreply.github.com>
2021-09-23 21:19:02 +0200

    Add files via upload

---
+++ R3_SQL_lundi.sql
@@ -0,0 +1,9 @@
+SELECT w.firstname, w.lastname, t.name, p.enrollment_date
+FROM wizard AS w
+LEFT JOIN player AS p
+on w.id= p.wizard_id
+LEFT JOIN team AS t
+on p.team_id=t.id
+WHERE dayofweek(p.enrollment_date) = 2
+AND t.name='Gryffindor'
+ORDER BY enrollment_date;
