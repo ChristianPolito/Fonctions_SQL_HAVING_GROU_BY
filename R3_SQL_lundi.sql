de42e08570397257900e44ab9e1609a4b333921d
Christian Polito <54096839+ChristianPolito@users.noreply.github.com>
2021-09-26 16:09:51 +0200

    Update R3_SQL_lundi.sql

--- R3_SQL_lundi.sql
+++ R3_SQL_lundi.sql
@@ -4,6 +4,6 @@ LEFT JOIN player AS p
 on w.id= p.wizard_id
 LEFT JOIN team AS t
 on p.team_id=t.id
-WHERE dayofweek(p.enrollment_date) = 2
+WHERE dayofweek(p.enrollment_date) = 0
 AND t.name='Gryffindor'
 ORDER BY enrollment_date ASC;
